927230d66ef727ee6f1b441366f7b5ff6f1b5874
Abishek <92878762+Abishek-G-Vincent@users.noreply.github.com>
2024-10-05 13:51:31 +0530

    Update nltk_utils.py
    stemming to lemmatization

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,19 +1,18 @@
 import nltk
 import numpy as np
+from nltk.stem import WordNetLemmatizer
 
-from nltk.stem.porter import PorterStemmer
-stemmer=PorterStemmer()
+lemmatizer = WordNetLemmatizer()
 
 def tokenize(sentence):
     return nltk.word_tokenize(sentence)
 
-def stem(word):
-    return stemmer.stem(word.lower())
+def lemmatize(word):
+    return lemmatizer.lemmatize(word.lower())
 
-def bag_of_words(tokenized_sentence,all_words):
-    tokenized_sentence = [stem(w) for w in tokenized_sentence]
-
-    bag = np.zeros(len(all_words),dtype=np.float32)
+def bag_of_words(tokenized_sentence, all_words):
+    tokenized_sentence = [lemmatize(w) for w in tokenized_sentence]  # Use lemmatization
+    bag = np.zeros(len(all_words), dtype=np.float32)
     for idx, w in enumerate(all_words):
         if w in tokenized_sentence:
             bag[idx] = 1.0
